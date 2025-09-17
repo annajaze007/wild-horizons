c1888e10693f0b68991ae7756d05cdd0cf8d99a3
annajaze007 <annajaze007@gmail.com>
2025-09-17 18:54:22 +0200

    retour à la première partie, on a fait le cas /api et le cas pas /api

--- server.js
+++ server.js
@@ -1,24 +1,50 @@
 import http from 'node:http'
-
+// PART 2
 const PORT = 8000
 
+import { getDataFromDB } from "./database/db.js"
+
+const destination = await getDataFromDB();
 
 const server = http.createServer((req, res) => {
+  //console.log(req.url)
+  // res.writeHead(200, { "Content-Type": "text/html" });
+  // res.end("<html><h1>The server is working</h1></html>")
 
-    console.log(req.url)
-
+  /*
+Challenge:
+1. Access the ‘setHeader’ method on the response object and pass in two strings to set the      
+   Content-Type to ‘application/json’ - watch out for casing! 
+*/
+   res.setHeader("Content-Type", "application/json")
+  if (req.url === '/api' && req.method === 'GET') {
+    res.statusCode = 200
+    res.end(JSON.stringify(destination))
+  }else{
     /*
-    Challenge:
-    Check the ‘url’ property on the req object. 
-    Only serve our string if it’s ‘/api’.
-    */
-
-    if (req.method === "GET") {
-        res.end('This is from the server')
-    }
-
-    else {res.end('This is not from the server')}
-
+Challenge:
+  1. If the client tries to access a route that isn’t covered by the above, send this object: 
+      {error: "not found", message: "The requested route does not exist"}
+  Think: what do we need to send along with the data?
+*/
+res.statusCode = 404
+res.end(JSON.stringify({error: "not found", message: "The requested route does not exist"}))
+  }
 })
 
 server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
+
+
+// const animal = {
+
+//   type: 'cattle',
+//   name: 'Yak Nicholson'
+
+// }
+
+// console.log(animal)
+
+// console.log(JSON.stringify(animal))
+
+// console.log(typeof JSON.stringify(animal))
+

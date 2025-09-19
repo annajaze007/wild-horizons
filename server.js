752baf395bbd7c83deb8ef848473bf8fc204efd8
annajaze007 <annajaze007@gmail.com>
2025-09-19 19:24:13 +0200

    acceder aux infos par continent

--- server.js
+++ server.js
@@ -7,28 +7,31 @@ import { getDataFromDB } from "./database/db.js"
 const destination = await getDataFromDB();
 
 const server = http.createServer((req, res) => {
-  //console.log(req.url)
-  // res.writeHead(200, { "Content-Type": "text/html" });
-  // res.end("<html><h1>The server is working</h1></html>")
 
-  /*
-Challenge:
-1. Access the ‘setHeader’ method on the response object and pass in two strings to set the      
-   Content-Type to ‘application/json’ - watch out for casing! 
-*/
-   res.setHeader("Content-Type", "application/json")
+  res.setHeader("Content-Type", "application/json")
+
   if (req.url === '/api' && req.method === 'GET') {
     res.statusCode = 200
     res.end(JSON.stringify(destination))
-  }else{
-    /*
-Challenge:
-  1. If the client tries to access a route that isn’t covered by the above, send this object: 
-      {error: "not found", message: "The requested route does not exist"}
-  Think: what do we need to send along with the data?
-*/
-res.statusCode = 404
-res.end(JSON.stringify({error: "not found", message: "The requested route does not exist"}))
+  }
+
+  else if (req.url.startsWith("/api/continent")) {
+    const continent = req.url.split('/').pop()
+    //const continent = req.url.substring(req.url.lastIndexOf('/') + 1)
+    console.log(continent)
+    const destinationFiltree = destination.filter(d => d.continent.toLowerCase() === continent.toLowerCase())
+    res.statusCode = 200
+    res.end(JSON.stringify(destinationFiltree))
+
+    /*  
+    2. If it does, serve only items from that continent.
+      (How can you get to what comes after the final slash?)
+      (What method can you use to filter data?)
+    */
+  }
+  else {
+    res.statusCode = 404
+    res.end(JSON.stringify({ error: "not found", message: "The requested route does not exist" }))
   }
 })
 
